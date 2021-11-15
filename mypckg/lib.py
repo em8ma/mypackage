6c3339f5229eb837b2ef32df84bcd9f49522c341
Emma Szonyi <emma.szonyi@gmail.com>
2021-11-15 13:47:40 +0100

    Initial Push

---
+++ mypckg/lib.py
@@ -0,0 +1,5 @@
+def try_me():
+    print('Mr Moony presents his compliments to Professor Snape, and begs him to keep his abnormally large nose out of other people''s business.')
+
+if __name__ == "__main__":
+    print(try_me())
